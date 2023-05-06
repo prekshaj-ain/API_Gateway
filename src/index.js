e7d3e85a4bf916c9afc3ce0a863833d356dad81a
preksha <jainpreksha2712@gmail.com>
2023-05-06 23:04:20 +0530

    addded rate-limiting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const { rateLimit } = require('express-rate-limit');
 
 dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000,
+	max: 5,
+})
 
 app.use(morgan('combined'));
+app.use(limiter);
 app.use('/bookingService', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true}));
 
 app.listen(process.env.PORT , ()=>{
