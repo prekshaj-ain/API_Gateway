0b28264c2960809cb8c79a678b63bf60fb02a682
preksha <jainpreksha2712@gmail.com>
2023-05-06 22:46:21 +0530

    setup basic proxy middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(morgan('combined'));
+app.use('/bookingService', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true}));
+
 app.listen(process.env.PORT , ()=>{
-    console.log(`Server started at ${PORT}`);
+    console.log(`Server started at ${process.env.PORT}`)
 } )
